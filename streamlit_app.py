2486b6a4c2f680cdb8e737aac89faec615763503
Tom Apedaile <apedaile.tom@gmail.com>
2025-01-12 18:40:01 -0700

    Update streamlit_app.py
    Expose the JSON data inside the response object.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ ingredients_list = st.multiselect(
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 if ingredients_list:
     #st.write(ingredients_list)
